a0a6d7acfabc11ce9f786f3f6c3dbd4d20e7f41b
Igor Trubin <itrubin.desire@gmail.com>
2023-02-03 09:25:47 +0300

    Игры разума этап 4

--- cli.js
+++ cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const name = readlineSync.question('Your answer: ');
-export function firstGame(userName) {
-    var readlineSync = require('readline-sync');
-    console.log("Welcome to the Brain Games!");
-    const userName= readlineSync.question("May I have your name? ");
-     
-    console.log(`Hello, ${userName}!`);
-    }
+function firstGame() {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+}
+
+firstGame();
