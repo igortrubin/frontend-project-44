0f9a4d72af9ea66fdcd197be9a4e56c5591de93b
Igor Trubin <itrubin.desire@gmail.com>
2023-02-16 22:43:43 +0300

    Проверка

--- src/random.js
+++ src/random.js
@@ -1,7 +1,6 @@
 const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+  return result;
 };
 
 export default getRandomInt;
